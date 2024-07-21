9dededd2d2f045296c77ffff6797a3c613d0909f
Avilov Ilya <demonizedpwnz@gmail.com>
2024-07-21 19:18:55 +0300

    Реализован BruteForce

--- src/Validator.java
+++ src/Validator.java
@@ -1,2 +1,29 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Validator {
+
+    /**
+     * Проверка ключа
+     */
+    public boolean isValidKey(int key, List<Character> alphabet) {
+        return key != 0 && key % alphabet.size() != 0;
+    }
+
+    /**
+     * Проверка существования файла
+     */
+    public boolean isFileExists(String filePath) {
+        Path path = Paths.get(filePath);
+        return Files.exists(path);
+    }
+
+    /**
+     * Проверка валидности алфавита
+     */
+    public boolean isValidAlphabet(List<Character> alphabet) {
+        return alphabet != null && !alphabet.isEmpty();
+    }
 }
